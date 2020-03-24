8e6e7e7992b888d4b41c79ed5ee1997760a975f7
Amr Saber <amr.m.saber.mail@gmail.com>
2020-03-24 23:23:23 +0200

    Refactor cli.js

--- src/helpers.js
+++ src/helpers.js
@@ -26,3 +26,27 @@ export function getValidCharacters(type) {
     default: throw new Error(`Unknown type [${type}]`);
   }
 }
+
+/**
+ * Returns a random string of the given type and length
+ *
+ * @param {Object} args
+ * @param {String} args.type
+ * @param {Number} args.length
+ *
+ * @returns {String}
+ */
+export function getRandomString({ type, length }) {
+  // Populate the valid letters based on the random string type
+  const valid = getValidCharacters(type);
+
+  // Create and construct the random numbers array
+  const randomLetters = [];
+  for (let i = 0; i < length; ++i) { randomLetters.push(getRandomElement(valid)); }
+
+  // Base64 string must have length that is a multiple of 4
+  while (type === TYPE_BASE_64 && randomLetters.length % 4 !== 0) { randomLetters.push('='); }
+
+  // Join the created random array to generate the resulting string
+  return randomLetters.join('');
+}
