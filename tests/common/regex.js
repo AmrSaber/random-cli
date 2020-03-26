78ad1d7fa5ee4fa357b8475277d3c4364d8bb4fc
Amr Saber <amr.m.saber.mail@gmail.com>
2020-03-26 23:51:05 +0200

    :heavy_check_mark: Add tests

---
+++ tests/common/regex.js
@@ -0,0 +1,7 @@
+export const hexRegex = /^[a-fA-F0-9]+$/;
+export const asciiRegex = /^[a-zA-Z0-9]+$/;
+export const base64Regex = /^[a-zA-Z0-9+/]+=*$/;
+export const numbersRegex = /^\d+$/;
+export const lettersRegex = /^[a-zA-Z]+$/;
+export const extendedRegex = /^[a-zA-Z0-9+\-_$#/@!]+$/;
+export const arrayRegex = /^(\d+ )+\d+$/;
