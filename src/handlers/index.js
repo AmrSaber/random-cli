35c620157dbfd79dbd2b186d2a98a8a89d39d620
Amr Saber <amr.m.saber.mail@gmail.com>
2020-04-09 04:44:13 +0200

    Handle pick command

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -3,3 +3,4 @@ export { default as numberHandler } from './number';
 export { default as floatHandler } from './float';
 export { default as arrayHandler } from './array';
 export { default as booleanHandler } from './boolean';
+export { default as pickHandler } from './pick';
